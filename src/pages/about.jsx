bb2c45c2790754268388dbed664f91addadc098c
Lokon Dey <lokondey72@gmail.com>
2025-04-03 13:37:49 +0600

    6s  3/4/25 commit you now

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -4,21 +4,7 @@ import { FaFacebook, FaGithub, FaLinkedin } from "react-icons/fa";
 const About = () => {
   return (
     <section id="about" className="container mx-auto px-4 py-10 text-center">
-      <div className="md:flex">
-        <div className="flex justify-center items-center md:min-h-screen">
-          <div className="relative group">
-            <img
-              src="/portfolio_img-1.jpg"
-              alt="Sample"
-              className="w-full h-auto rounded-full transition-transform duration-300 ease-in-out group-hover:scale-110 group-hover:shadow-xl animate-zoomIn"
-            />
-            <div className="absolute top-0 left-0 w-full h-full bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out flex justify-center items-center">
-              <p className="text-indigo-600 text-2xl font-semibold">
-                Rikto Dey Lokon
-              </p>
-            </div>
-          </div>
-        </div>
+      <div className="md:flex items-center">
         <div className="p-20">
           <div>
             <h2 className="text-3xl font-semibold text-indigo-600">About Me</h2>
@@ -50,6 +36,20 @@ const About = () => {
             >
               Contact Me
             </a>
+          </div>
+        </div>
+        <div className="flex justify-center items-center md:min-h-screen">
+          <div className="relative group">
+            <img
+              src="/portfolio_img-1.jpg"
+              alt="Sample"
+              className="w-full h-auto rounded-full transition-transform duration-300 ease-in-out group-hover:scale-110 group-hover:shadow-xl animate-zoomIn"
+            />
+            <div className="absolute top-0 left-0 w-full h-full bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out flex justify-center items-center">
+              <p className="text-white text-2xl font-semibold">
+                Rikto Dey Lokon
+              </p>
+            </div>
           </div>
         </div>
       </div>
